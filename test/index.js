974bfa5139df86c23e6d16a888442df50e817307
Mike Chaliy <mike@chaliy.name>
2016-04-29 20:58:52 +0300

    Added test function #3

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
-module.exports = function(context, req) {
-    context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
+module.exports = (context, req) => {
+    context.log(`Node.js HTTP trigger function processed a request. ${req.originalUrl}`);
     
     var name = req.query.name || "foo";
     
